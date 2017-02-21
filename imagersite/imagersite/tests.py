de0d9daa038dfba535ca5d6683ad7c83679c7627
regenalgrant <regenal@mac.com>
2017-02-20 22:54:36 -0800

    test for new user

--- imagersite/imagersite/tests.py
+++ imagersite/imagersite/tests.py
@@ -1,13 +1,18 @@
 """Testing for registration and login."""
 from django.test import Client, TestCase, RequestFactory
 from django.contrib.auth.models import User
-from django
+
 
 class FrontEndTestCase(TestCase):
     """Test for registration and login."""
     def setUp(self):
         self.client = Client()
         self.request = RequestFactory()
+        self.new_user = User.objects.create_user(
+            username = 'regenal',
+            email = 'regenal@gmail.com',
+            password = 'zaq14321'
+        )
 
     def test_home_view_has_200(self):
         """Test for home view has a 200 status."""
@@ -27,9 +32,14 @@ class FrontEndTestCase(TestCase):
         response = self.client.get('/login/')
         self.assertTrue(response.status_code == 200)
 
-    def test_registered_user(self):
-        """Test a user is created."""
-        self.register_user()
-        self.assertTrue(User.objects.count() == 0)
+    def test_registered_new_user(self):
+        """Test a new user is created."""
+        self.assertFalse(User.objects.count() == 0)
+        self.new_user
         self.assertTrue(User.objects.count() == 1)
-        pass
+
+    # def test_registered_user_is_not_active(self):
+    #     """Test a newly registered user is inactive."""
+    #     self.user()
+    #     self.client = User.objects.first()
+    #     self.assertFalse(self.client.is_active) # ---not working, no attribute is_active
